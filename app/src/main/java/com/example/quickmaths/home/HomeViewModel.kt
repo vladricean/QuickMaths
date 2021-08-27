a315648a31f1dc1810282c722c8cd1fda6792e08
ricean <ioan.ricean@vspartners.us>
2021-08-27 15:25:37 +0300

    Navigating from Home to Play

--- app/src/main/java/com/example/quickmaths/home/HomeViewModel.kt
+++ app/src/main/java/com/example/quickmaths/home/HomeViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.quickmaths.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _navigateToPlay = MutableLiveData<Boolean>()
+    val navigateToPlay: LiveData<Boolean>
+        get() = _navigateToPlay
+
+    fun onPlayClicked(){
+        _navigateToPlay.value = true
+    }
+
+    fun onNavigatedToPlay(){
+        _navigateToPlay.value = false
+    }
 }

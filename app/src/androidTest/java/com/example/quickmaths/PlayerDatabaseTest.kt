291c53f1fe8d0e6b5dc7e988d91820864bddb523
ricean <ioan.ricean@vspartners.us>
2021-09-02 18:57:18 +0300

    Implemented entity, dao, database

---
+++ app/src/androidTest/java/com/example/quickmaths/PlayerDatabaseTest.kt
@@ -0,0 +1,51 @@
+package com.example.quickmaths
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.quickmaths.database.Player
+import com.example.quickmaths.database.PlayerDatabase
+import com.example.quickmaths.database.PlayerDatabaseDao
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+
+@RunWith(AndroidJUnit4::class)
+class PlayerDatabaseTest {
+
+    private lateinit var playerDao : PlayerDatabaseDao
+    private lateinit var db: PlayerDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, PlayerDatabase::class.java)
+                // Allowing main thread queries, just for testing.
+                .allowMainThreadQueries()
+                .build()
+        playerDao = db.playerDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+//        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertPlayer() {
+        val player = Player()
+        playerDao.insert(player)
+        val somePlayer = playerDao.getPlayer()
+        assertEquals(somePlayer?.score, null)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/quickmaths/database/PlayerDatabaseDao.kt
@@ -0,0 +1,33 @@
+package com.example.quickmaths.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface PlayerDatabaseDao{
+    @Insert
+    fun insert(player: Player){
+
+    }
+
+    @Update
+    fun update(player: Player){
+
+    }
+
+    @Query("SELECT * from player_name_score_table WHERE playerId = :key")
+    fun get(key: Long): Player?
+
+    @Query("DELETE FROM player_name_score_table")
+    fun clear()
+
+    // First player created
+    @Query("SELECT * FROM player_name_score_table ORDER BY playerId DESC LIMIT 1")
+    fun getPlayer(): Player?
+
+    @Query("SELECT * FROM player_name_score_table ORDER BY playerId DESC")
+    fun getAllPlayers(): LiveData<List<Player>>
+}

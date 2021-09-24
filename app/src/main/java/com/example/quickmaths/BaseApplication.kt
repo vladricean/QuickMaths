5cc425ca1da07549b1261ef7bff3bf0ab694a425
vlad <ioan.ricean@vspartners.us>
2021-09-24 16:34:46 +0300

    Added Timber Library

--- app/src/main/java/com/example/quickmaths/BaseApplication.kt
+++ app/src/main/java/com/example/quickmaths/BaseApplication.kt
@@ -2,6 +2,15 @@ package com.example.quickmaths
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApplication : Application() {  }
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Planting Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}

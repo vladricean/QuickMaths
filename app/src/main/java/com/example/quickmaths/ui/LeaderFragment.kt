8d9b0180d690f91545000d79ce263354c5590b46
ricean <ioan.ricean@vspartners.us>
2021-09-29 13:54:20 +0300

    Put data into Adapter from the ViewModel

--- app/src/main/java/com/example/quickmaths/ui/LeaderFragment.kt
+++ app/src/main/java/com/example/quickmaths/ui/LeaderFragment.kt
@@ -5,32 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.quickmaths.R
+import com.example.quickmaths.database.PlayerDatabase
 import com.example.quickmaths.databinding.LeaderFragmentBinding
 import com.example.quickmaths.recycler_adapters.PlayerStatsAdapter
 import com.example.quickmaths.viewmodels.LeaderViewModel
+import com.example.quickmaths.viewmodelsfactory.LeaderViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class LeaderFragment : Fragment() {
 
-    private lateinit var viewModel: LeaderViewModel
     private lateinit var binding: LeaderFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = LeaderFragmentBinding.inflate(inflater)
-        viewModel = ViewModelProvider(this).get(LeaderViewModel::class.java)
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.leader_fragment, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = PlayerDatabase.getInstance(application).playerDatabaseDao
+        val viewModelFactory = LeaderViewModelFactory(dataSource, application)
+
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(LeaderViewModel::class.java)
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
 
         val adapter = PlayerStatsAdapter()
         binding.playersList.adapter = adapter
 
+        viewModel.players.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                adapter.data = it
+            }
+        })
+
         return binding.root
     }
+
+
 
 }
--- app/src/main/java/com/example/quickmaths/viewmodels/LeaderViewModel.kt
+++ app/src/main/java/com/example/quickmaths/viewmodels/LeaderViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.quickmaths.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import com.example.quickmaths.database.PlayerDatabaseDao
 
-class LeaderViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LeaderViewModel(
+    dataSource: PlayerDatabaseDao,
+    application: Application
+) : ViewModel() {
+
+    val database = dataSource
+
+    val players = database.getAllPlayers()
 }

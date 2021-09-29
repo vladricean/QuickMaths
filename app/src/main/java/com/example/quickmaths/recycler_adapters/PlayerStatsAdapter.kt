16ef977e1229227e15a2523ba6380b24cd73708d
ricean <ioan.ricean@vspartners.us>
2021-09-29 18:46:08 +0300

    Using data binding with RecyclerView
    1. Added data binding to the layout file
    2. Inflated the item layout using data binding inside Adapter
    3. Removed declared properties inside the Adaptor inside ViewHolder class

--- app/src/main/java/com/example/quickmaths/recycler_adapters/PlayerStatsAdapter.kt
+++ app/src/main/java/com/example/quickmaths/recycler_adapters/PlayerStatsAdapter.kt
@@ -1,16 +1,13 @@
 package com.example.quickmaths.recycler_adapters
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quickmaths.R
 import com.example.quickmaths.database.Player
+import com.example.quickmaths.databinding.ListItemPlayerStatsBinding
 
 class PlayerStatsAdapter : ListAdapter<Player, PlayerStatsAdapter.ViewHolder>(PlayerDiffCallback()) {
 
@@ -23,16 +20,12 @@ class PlayerStatsAdapter : ListAdapter<Player, PlayerStatsAdapter.ViewHolder>(Pl
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val playerName: TextView = itemView.findViewById(R.id.player_name)
-        val playerScore: TextView = itemView.findViewById(R.id.player_score)
-        val playerImage: ImageView = itemView.findViewById(R.id.player_image)
+    class ViewHolder private constructor(val binding: ListItemPlayerStatsBinding) : RecyclerView.ViewHolder(binding.root){
 
-        fun bind(item: Player){
-//            val res = itemView.context.resources
-            playerName.text = item.name
-            playerScore.text = item.score.toString()
-            playerImage.setImageResource(
+        fun bind(item: Player) {
+            binding.playerName.text = item.name
+            binding.playerScore.text = item.score.toString()
+            binding.playerImage.setImageResource(
                 if (item.score <= 1) {
                     R.drawable.ic_launcher_background
                 } else {
@@ -44,8 +37,8 @@ class PlayerStatsAdapter : ListAdapter<Player, PlayerStatsAdapter.ViewHolder>(Pl
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.list_item_player_stats, parent, false)
-                return ViewHolder(view)
+                val binding = ListItemPlayerStatsBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }

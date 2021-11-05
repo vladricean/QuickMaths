1a214b9a38a2e72460a815fcb2cb94e8f83f07ca
Vlad <vlad.ricean@brivo.com>
2021-11-05 17:50:55 +0200

    -removed unused libraries

--- app/src/main/java/com/example/quickmaths/ui/HomeFragment.kt
+++ app/src/main/java/com/example/quickmaths/ui/HomeFragment.kt
@@ -1,10 +1,7 @@
 package com.example.quickmaths.ui
 
-import android.content.Context
-import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.example.quickmaths.MainActivity
 import com.example.quickmaths.R
 import com.example.quickmaths.databinding.HomeFragmentBinding
 import com.example.quickmaths.viewmodels.HomeViewModel
@@ -20,7 +16,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint

3b56cda39ab6493dba6cacba9a6b910ce710a8c4
Vlad <vlad.ricean@brivo.com>
2021-12-17 15:43:14 +0200

    -moved more logic to view model

--- app/src/main/java/com/example/quickmaths/ui/SignInActivity.kt
+++ app/src/main/java/com/example/quickmaths/ui/SignInActivity.kt
@@ -3,7 +3,6 @@ package com.example.quickmaths.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -11,21 +10,16 @@ import androidx.lifecycle.ViewTreeLifecycleOwner
 import com.example.quickmaths.MainActivity
 import com.example.quickmaths.R
 import com.example.quickmaths.databinding.ActivitySignInBinding
-import com.example.quickmaths.enums.ConfirmButtonState
 import com.example.quickmaths.viewmodels.SignInViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_sign_in.*
-import timber.log.Timber
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var viewModel: SignInViewModel
-    private lateinit var db: FirebaseFirestore
-    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,72 +31,23 @@ class SignInActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         ViewTreeLifecycleOwner.set(window.decorView, this)
 
-        mAuth = FirebaseAuth.getInstance()
-        db = Firebase.firestore
-
         setupObservers()
     }
 
     private fun setupObservers() {
         viewModel.editedText.observe(this,
-        Observer { username ->
-            viewModel.setupConfirmButtonState(username)
-        })
+            Observer { username ->
+                viewModel.setupConfirmButtonState(username)
+            })
         viewModel.onConfirmPressed().observe(this,
-        Observer {
-            btn_confirm.setEnabled(false)
-            checkIfUsernameExists(viewModel.editedText.value)
-        })
-    }
-
-    private fun checkIfUsernameExists(username: String?) {
-        db.collection("users")
-            .whereEqualTo("name", username)
-            .get()
-            .addOnSuccessListener { documents ->
-                if(documents.isEmpty){
-                    signInAnonymously(username)
-                }else{
-                    btn_confirm.setEnabled(false)
-                }
-            }
-            .addOnFailureListener{ exception ->
-                Timber.w("Error getting documents: ${exception}")
-            }
-    }
-
-    private fun signInAnonymously(username: String?) {
-        mAuth.signInAnonymously()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    addFreshUser(username)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
-
-    private fun addFreshUser(username: String?) {
-        val user = hashMapOf(
-            "name" to username,
-            "score" to 0
-        )
-        val currentUser = mAuth.currentUser
-        db.collection("users").document(currentUser!!.uid)
-            .set(user)
-            .addOnSuccessListener { documentReference ->
-                Timber.i("User ${currentUser.displayName} has been added to firestore")
-            }
-            .addOnFailureListener { e ->
-                Timber.w("Error adding document", e)
-            }
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+            Observer {
+                viewModel.checkIfUsernameExists()
+            })
+        viewModel.onNavigateToMain().observe(this,
+            Observer {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
     }
 }
